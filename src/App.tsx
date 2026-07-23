5052e9dcaa2fdbbd98673dcc67d76a5d37b94c78
ananya mangistu <ananyasmengistu06@gmail.com>
2026-07-23 01:23:42 -0700

    fix: update routing structure to include login path and improve navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-
-
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import ProtectedRoute from './hooks/ProtectedRoute';
 import Login from './pages/Login';
 import Home from './pages/Home';
-export default function App() {
-  return (
-    <Routes>
-      <Route path='/' element={<Login/>}/>
-       <Route element={<ProtectedRoute />}>
-        <Route path='/home' element={<Home/>}/>
-       
-       
-       </Route>
 
-    </Routes>
-  );
+export default function App() {
+return (
+  <Routes>
+    <Route path="/login" element={<Login />} />
+
+    <Route element={<ProtectedRoute />}>
+      <Route path="/home" element={<Home />} />
+    </Route>
+
+    <Route path="/" element={<Navigate to="/home" replace />} />
+    <Route path="*" element={<Navigate to="/home" replace />} />
+  </Routes>
+);
 }
